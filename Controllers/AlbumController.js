10653c1343956e4c6db4a539ecb4c4c29478209b
Maxime <maxilefranc@gmail.com>
2023-10-12 10:41:04 -0500

    Now server doesn't crash when a empty album title is given
    used Try/Catch for insert data in db // WIP: show a  message

--- Controllers/AlbumController.js
+++ Controllers/AlbumController.js
@@ -1,11 +1,21 @@
+import Album from '../Models/Album.js';
+
 const AlbumController = {};
 
 AlbumController.createAlbumForm = (req, res) => {
   res.render('new-album', { title: 'Nouvel album' });
 };
 
-AlbumController.createAlbum = (req, res) => {
-  res.send('OK');
+AlbumController.createAlbum = async (req, res) => {
+  const { albumTitle } = req.body;
+  try {
+    await Album.create({
+      title: albumTitle,
+    });
+    res.redirect('/');
+  } catch (err) {
+    res.redirect('/albums/create');
+  }
 };
 
 export default AlbumController;
